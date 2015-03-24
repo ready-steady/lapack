7b2b499e0a1893558aaac28449dc57155951d038
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-03-24 15:31:19 +0100

    Move the code from linear/lapack

---
+++ main.go
@@ -0,0 +1,6 @@
+// Package lapack provides an interface to the Linear Algebra PACKage, which
+// also includes the Basic Linear Algebra Subprograms.
+//
+// http://www.netlib.org/lapack/
+// http://www.netlib.org/blas/
+package lapack
