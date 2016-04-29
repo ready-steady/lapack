de32b8499443899235989a725c4c30699e742b2f
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-04-29 13:47:00 +0200

    Add DGETRF and DGETRI

--- lapack.go
+++ lapack.go
@@ -6,14 +6,25 @@ import "C"
 
 import "unsafe"
 
-// http://www.netlib.org/lapack/explore-html/d1/db3/dgtsv_8f.html
+func DGETRI(n int, a []float64, lda int, ipiv []int, work []float64, lwork int,
+	info *int) {
+
+	C.dgetri(C.int(n), (*C.double)(&a[0]), C.int(lda),
+		(*C.int)(unsafe.Pointer(&ipiv[0])), (*C.double)(&work[0]), C.int(lwork),
+		(*C.int)(unsafe.Pointer(info)))
+}
+
+func DGETRF(m, n int, a []float64, lda int, ipiv []int, info *int) {
+	C.dgetrf(C.int(m), C.int(n), (*C.double)(&a[0]), C.int(lda),
+		(*C.int)(unsafe.Pointer(&ipiv[0])), (*C.int)(unsafe.Pointer(info)))
+}
+
 func DGTSV(n, nrhs int, dl, d, du, b []float64, ldb int, info *int) {
 	C.dgtsv(C.int(n), C.int(nrhs), (*C.double)(&dl[0]), (*C.double)(&d[0]),
 		(*C.double)(&du[0]), (*C.double)(&b[0]), C.int(ldb),
 		(*C.int)(unsafe.Pointer(info)))
 }
 
-// http://www.netlib.org/lapack/explore-html/dd/d4c/dsyev_8f.html
 func DSYEV(jobz, uplo byte, n int, a []float64, lda int, w, work []float64,
 	lwork int, info *int) {
 
--- lapack.h
+++ lapack.h
@@ -1,11 +1,26 @@
 #ifndef LAPACK_H
 #define LAPACK_H
 
+extern void dgetrf_(int *m, int *n, double *a, int *lda, int *ipiv, int *info);
+
+extern void dgetri_(int *n, double *a, int *lda, int *ipiv, double *work,
+	int *lwork, int *info);
+
 extern void dgtsv_(const int *n, const int *nrhs, const double *dl, double *d,
 	double *du, double *b, int *ldb, int *info);
 
 extern void dsyev_(const char *jobz, const char *uplo, const int *n, double *a,
 	const int *lda, double *w, double *work, const int *lwork, int *info);
+
+void dgetri(int n, double *a, int lda, int *ipiv, double *work, int lwork,
+	int *info) {
+
+	dgetri_(&n, a, &lda, ipiv, work, &lwork, info);
+}
+
+void dgetrf(int m, int n, double *a, int lda, int *ipiv, int *info) {
+	dgetrf_(&m, &n, a, &lda, ipiv, info);
+}
 
 void dgtsv(int n, int nrhs, double *dl, double *d, double *du, double *b,
 	int ldb, int *info) {
